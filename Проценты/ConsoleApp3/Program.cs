a2a2b669299cf502978e61260d52774a7febdd4d
slavaerto1 <33775498+slavaerto1@users.noreply.github.com>
2017-11-24 23:00:05 +0500

    Update Program.cs

--- Проценты/ConsoleApp3/Program.cs
+++ Проценты/ConsoleApp3/Program.cs
@@ -10,6 +10,15 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
+         Console.WriteLine("Сначала сумму потом проценты затем число месяцев");
+            string a;
+            a = Console.ReadLine();
+            string d;
+           d = Console.ReadLine();
+            string c;
+            c = Console.ReadLine();
+            var t;
+            Console.WriteLine(value:(t = (((a / 100) * d) * c) + a)); 
         }
     }
 }
